db31e1e8b1e38384c109d643cafb52a473093813
alext <alekey.tereshkin@mail.ru>
2022-08-24 15:02:49 +0300

    add new

---
+++ src/main/java/org/example/InfoTab.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class InfoTab {
+    private String keyWords;
+    private String expereinceAmount;
+    private String regionId;
+    public void setKeyWords(String keyWords){this.keyWords=keyWords;}
+    public void setExpereinceAmount(String experienceAmount){this.expereinceAmount=experienceAmount;}
+    public void setRegionId(String regionId ){this.regionId=regionId;}
+    public String getKeyWords(){return this.keyWords;}
+    public String getExpereinceAmount(){return this.expereinceAmount;}
+    public String getRegionId(){return this.regionId;}
+}
